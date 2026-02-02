77c89abd184ac4ca53cf55898cbee5020ded5cc3
clement.mondary <clement.mondary@edu.ecole-89.com>
2026-02-02 11:32:34 +0100

    web browser localstorage handling

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { CONFIG } from "@/constants/Config";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SecureStore from "expo-secure-store";
 import type React from "react";
 import {
@@ -8,6 +9,7 @@ import {
   useEffect,
   useState,
 } from "react";
+import { Platform } from "react-native";
 
 interface User {
   id: number;
@@ -45,20 +47,36 @@ const REFRESH_TOKEN_KEY = "refreshToken";
 
 // Helpers outside component to ensure stability
 const saveTokens = async (accessToken: string, refreshToken: string) => {
-  await SecureStore.setItemAsync(ACCESS_TOKEN_KEY, accessToken);
-  await SecureStore.setItemAsync(REFRESH_TOKEN_KEY, refreshToken);
+  if (Platform.OS === "web") {
+    await AsyncStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+    await AsyncStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+  } else {
+    await SecureStore.setItemAsync(ACCESS_TOKEN_KEY, accessToken);
+    await SecureStore.setItemAsync(REFRESH_TOKEN_KEY, refreshToken);
+  }
 };
 
 const clearTokens = async () => {
-  await SecureStore.deleteItemAsync(ACCESS_TOKEN_KEY);
-  await SecureStore.deleteItemAsync(REFRESH_TOKEN_KEY);
+  if (Platform.OS === "web") {
+    await AsyncStorage.removeItem(ACCESS_TOKEN_KEY);
+    await AsyncStorage.removeItem(REFRESH_TOKEN_KEY);
+  } else {
+    await SecureStore.deleteItemAsync(ACCESS_TOKEN_KEY);
+    await SecureStore.deleteItemAsync(REFRESH_TOKEN_KEY);
+  }
 };
 
 const getAccessToken = async () => {
+  if (Platform.OS === "web") {
+    return await AsyncStorage.getItem(ACCESS_TOKEN_KEY);
+  }
   return await SecureStore.getItemAsync(ACCESS_TOKEN_KEY);
 };
 
 const getRefreshToken = async () => {
+  if (Platform.OS === "web") {
+    return await AsyncStorage.getItem(REFRESH_TOKEN_KEY);
+  }
   return await SecureStore.getItemAsync(REFRESH_TOKEN_KEY);
 };
 
