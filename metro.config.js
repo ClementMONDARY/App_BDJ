5ef8eb6514511083f025822055efe78011823050
Clément MONDARY <clement.mondary@edu.ecole-89.com>
2026-05-31 16:47:21 +0200

    feat: + account page content + calendar config fix (#39)

---
+++ metro.config.js
@@ -0,0 +1,19 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (
+    moduleName === './Profiler' &&
+    context.originModulePath.includes('react-native-calendars')
+  ) {
+    return {
+      type: 'sourceFile',
+      filePath: path.resolve(__dirname, 'shims/CalendarsProfiler.js'),
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+module.exports = config;
